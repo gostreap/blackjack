141d0676bcd081a807a3cb81716bda963d9617bc
Tristan François <tpv.francois@gmail.com>
2022-03-02 14:34:06 +0100

    Cleaned q_learning

--- src/q_learning.py
+++ src/q_learning.py
@@ -1,19 +1,20 @@
+import random
+
 import gym
 import numpy as np
-import random
 
 
 class QLearning:
     def __init__(self, epsilon=0.1, gamma=0.5) -> None:
+        self.env = gym.make("Blackjack-v1")
         self.gamma = gamma
         self.epsilon = epsilon
-        self.env = gym.make("Blackjack-v1")
         self.Q = np.zeros(
             (
                 self.env.observation_space[0].n,
                 self.env.observation_space[1].n,
                 self.env.observation_space[2].n,
-                self.env.action_space.n
+                self.env.action_space.n,
             )
         )
         self.count = self.Q.copy()
@@ -29,17 +30,14 @@ class QLearning:
         else:
             return self.greedy_policy(observation)
 
-    def greedy_policy(self, observation, debug=False):
+    def greedy_policy(self, observation):
         hit = self.Q[self.get_observation_int(observation)][1]
         stick = self.Q[self.get_observation_int(observation)][0]
 
-        if debug:
-            print(hit, stick)
-
         if hit > stick:
-            return 0
-        elif stick > hit:
             return 1
+        elif stick > hit:
+            return 0
         else:
             return self.env.action_space.sample()
 
@@ -47,48 +45,39 @@ class QLearning:
         # Training
         observation = self.env.reset()
         for i in range(n_episode):
-            if i % 10000 == 0:
-                print(i)
-                # print(np.sum(self.Q))
+            if i % 50000 == 0:
+                print("training, episode {} out of {}".format(i, n_episode))
+
             done = False
             while not done:
                 action = self.epsilon_greedy_policy(observation)
                 next_observation, reward, done, info = self.env.step(action)
-                
+
                 observation_int = self.get_observation_int(observation)
                 next_observation_int = self.get_observation_int(observation)
-                
-                
-                self.count[observation_int][action] += 1 
-                alpha = 1/self.count[observation_int][action]
+
+                self.count[observation_int][action] += 1
+                alpha = 1 / self.count[observation_int][action]
 
                 maxvalue = max(self.Q[next_observation_int][0], self.Q[next_observation_int][1])
-                self.Q[observation_int][action] = (1 - alpha) * self.Q[observation_int][action] + alpha * (reward + self.gamma * maxvalue)
-                # self.Q[observation_int][action] = 1
-                # print(self.Q[observation_int][action])               
-                # print(observation, action, self.Q[observation_int][action])
-                # print(self.Q)
+                self.Q[observation_int][action] = (1 - alpha) * self.Q[observation_int][action] + alpha * (
+                    reward + self.gamma * maxvalue
+                )
+
                 observation = next_observation
 
             observation = self.env.reset()
-        # print(self.Q)
 
     def test(self, n_episode):
         # Test
-        win, draw, loss= 0, 0, 0
-        count_hit, count_stick = 0, 0
+        win, draw, loss = 0, 0, 0
         observation = self.env.reset()
         for _ in range(n_episode):
             done = False
             reward = 0
             while not done:
-                action = self.greedy_policy(observation, debug=True)
-                # action = 0
-                # action = self.env.action_space.sample()
-                print("action", observation, action)
+                action = self.greedy_policy(observation)
                 next_observation, reward, done, info = self.env.step(action)
-                # print(next_observation, reward)
-
                 observation = next_observation
 
             if reward == 0:
@@ -96,27 +85,14 @@ class QLearning:
             elif reward > 0:
                 win += 1
             else:
-                loss+=1
-            
-            observation = self.env.reset()
-            
-        print(win/ n_episode, draw/n_episode, loss/n_episode)
+                loss += 1
 
-def printQ(Q):
-    for i in range(len(Q)):
-        print(i, np.sum(Q[i]))
-        # for j in range(len(Q[i])):
-        #     for k in range(len(Q[i, j])):
-        #         for l in range(len(Q[i, j, k])):
-        #             print(Q[i, j, k, l])
+            observation = self.env.reset()
+
+        print(win / n_episode, draw / n_episode, loss / n_episode)
 
 
 if __name__ == "__main__":
     qlearning = QLearning()
-    qlearning.train(100000)
-    qlearning.test(1000)
-    print(20, qlearning.Q[20])
-    # print(np.sum(qlearning.Q))
-    # printQ(qlearning.Q)
-
-
+    qlearning.train(300000)
+    qlearning.test(20000)
